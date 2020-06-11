c0394ab8564c8e0a5f86af78b3d690a37c620156
Sean Froste <darrenkarthik20000@gmail.com>
2020-06-11 20:15:50 +0530

    11/6/20 commit

---
+++ WackyBreakout/Assets/Scripts/Gameplay/Paddle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.Jobs;
+/// <summary>
+/// paddle behaviour script
+/// </summary>
+public class Paddle : MonoBehaviour
+{
+    Rigidbody2D PaddleRB;
+    BoxCollider2D bc2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D PaddleRB = GetComponent<Rigidbody2D>();
+        bc2d = GetComponent<BoxCollider2D>();
+        float paddleColliderHalf = bc2d.size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Physics Updates happen this frequently
+    public void FixedUpdate()
+    {
+        
+    }
+
+    public float calculateClampedX(float x)
+    {
+        if (x - paddleColliderHalf < ScreenUtils.ScreenLeft || x + paddleColliderHalf > ScreenUtils.ScreenRight)
+        {
+            return x;
+        }
+        else
+        {
+            return transform.position.x;
+        }
+    }
+}
